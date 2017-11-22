a71dc6d887f540498890a26f4658882fabfa6c8e
Einar Persson <einar.vilhelm.persson@gmail.com>
2017-11-22 11:55:02 +0100

    Add project files.

---
+++ ConsoleStenSaxPåse/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleStenSaxPåse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
